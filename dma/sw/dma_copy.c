fb148ab9aaa3b95a34a3fe9b02461092c0fed1bd
dman <djolin1@uncc.edu>
2025-03-10 22:03:06 -0400

    c code from creating dma in HLS

---
+++ dma/sw/dma_copy.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#define BURST_LEN 16  // example burst length (can tune as needed)
+
+// Top-level HLS function for DMA
+void dma_copy(volatile uint32_t *src, volatile uint32_t *dst, uint32_t length) {
+#pragma HLS INTERFACE m_axi port=src  offset=slave bundle=AXI_SRC
+#pragma HLS INTERFACE m_axi port=dst  offset=slave bundle=AXI_DST
+#pragma HLS INTERFACE s_axilite port=src   bundle=CTRL
+#pragma HLS INTERFACE s_axilite port=dst   bundle=CTRL
+#pragma HLS INTERFACE s_axilite port=length bundle=CTRL
+#pragma HLS INTERFACE s_axilite port=return bundle=CTRL
+//#pragma HLS PIPELINE II=1   // (optional) pipeline loop for better burst performance
+
+    // Simple memory copy loop
+    for (uint32_t i = 0; i < length; ++i) {
+        uint32_t data = src[i];
+        dst[i] = data;
+    }
+}
+
